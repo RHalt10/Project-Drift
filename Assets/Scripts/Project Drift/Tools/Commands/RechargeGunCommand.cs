0af23a93999743571f0b34383faa9a6a84758eb0
Li <jasli@umich.edu>
2023-01-20 23:10:52 -0500

    Recharge gun command

---
+++ Assets/Scripts/Project Drift/Tools/Commands/RechargeGunCommand.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WSoft.Tools.Console;
+
+public class RechargeGun : ConsoleCommand
+{
+    public RechargeGun()
+    {
+        commandWord = "rechargeGun";
+    }
+
+    public override List<string> GetValidArgs()
+    {
+        return new List<string>();
+    }
+
+    public override bool Process(string[] args)
+    {
+        PlayerGun gunComponent = PlayerController.Instance.GetComponent<PlayerGun>();
+        int ammoAmount = gunComponent.currentWeapon.maxAmmo;
+        if(args.Length > 0)
+        {
+            int inputAmmo;
+            if (int.TryParse(args[0], out inputAmmo))
+            {
+                ammoAmount = inputAmmo;
+            } else
+            {
+                Debug.Log("Invalid ammo amount.");
+                return false;
+            }
+        }
+        //Set ammo
+        gunComponent.currentAmmo = ammoAmount;
+
+        return true;
+    }
+}
