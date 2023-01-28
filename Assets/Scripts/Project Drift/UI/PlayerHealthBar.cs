afe9e2903e05e8d865762ebdbfe91d01f08878f2
henrlin24 <henrlin@umich.edu>
2023-01-27 23:09:05 -0500

    Created test scene for health UI. Got a lerp to work, although there are some bugs at the moment in regards in subsequent lerps getting really slow. Also need to implement movement back in the coroutine, hence creating a "shake" movement.

--- Assets/Scripts/Project Drift/UI/PlayerHealthBar.cs
+++ Assets/Scripts/Project Drift/UI/PlayerHealthBar.cs
@@ -46,4 +46,37 @@ public class PlayerHealthBar : MonoBehaviour
             spawnedPrefab.GetComponent<Image>().color = i >= playerHealth.Current ? invalidColor : validColor;
         }
     }
+
+    // used to test out ShakeUI()
+    private void Update()
+    {
+        // use U key to trigger shake event
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("Pressed U");
+            StartCoroutine(ShakeUI());
+        }
+    }
+
+    // public variables to allow for changing the shake parameters for testing
+    public float shakeAmount = 10.0f;
+    public float lerpTime = 1.0f;
+    // coroutine to lerp ui element to produce shaking effect
+    private IEnumerator ShakeUI()
+    {
+        Transform UITransform = GetComponent<RectTransform>().transform;
+        Vector3 StartPos = UITransform.position;
+        Vector3 EndPos = new Vector3(UITransform.position.x + 10.0f, UITransform.position.y + 10.0f, UITransform.position.z);
+        
+        float startTime = Time.time;
+        float proportion = (Time.time - startTime) / (startTime + lerpTime);
+        while (proportion < 1.0f)
+        {
+            UITransform.position = new Vector3(Mathf.Lerp(StartPos.x, EndPos.x, proportion), Mathf.Lerp(StartPos.y, EndPos.y, proportion), StartPos.z);
+            proportion = (Time.time - startTime) / (startTime + lerpTime);
+            yield return null;
+        }
+        UITransform.position = EndPos;
+    }
+
 }
