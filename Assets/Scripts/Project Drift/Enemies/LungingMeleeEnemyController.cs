55782ffa06c5c2cc195bc5d2731e75feb2cd45c2
bobzhang-stack <bobzhang@umich.edu>
2023-01-29 06:55:04 -0500

    Added an avoid obstacle behavior. It could use improvements since the current way to is to check in 4 directions (45 & 90 degrees clockwise & counterclockwise relative to direction towards player) to decide which of 4 to use and go around obstacle. There are some TODOs written that are mostly for me needing to ask the design team later for specifics. Main functionality of the lunging melee enemy should work (from what I tested).

--- Assets/Scripts/Project Drift/Enemies/LungingMeleeEnemyController.cs
+++ Assets/Scripts/Project Drift/Enemies/LungingMeleeEnemyController.cs
@@ -49,8 +49,7 @@ public class LungingMeleeEnemyController : MonoBehaviour
     public float attackWindup = 1.0f;
 
     //can't use forces with groundCharacterController so doing lunge by time & top speed
-    //public float lungeForce = 100.0f;
-    //TODO: decides the duration and top speed of the lunge
+    //decides the duration and top speed of the lunge
     public float lungeDuration = 1.0f; //temp values
     public float lungeTopSpeed = 5.0f; //temp values
 
@@ -93,7 +92,6 @@ public class LungingMeleeEnemyController : MonoBehaviour
 
             //check if a nearby obstacle is blocking its attack range detection
             bool obstacleInFront = Physics2D.Raycast(this.transform.position, direction, attackRange, LayerMask.GetMask("Obstacle"));
-
             if (obstacleInFront)
             {
                 State = EnemyState.AvoidObstacle;
@@ -107,7 +105,7 @@ public class LungingMeleeEnemyController : MonoBehaviour
             yield return null;
         }
 
-        //TODO: there is a flaw where enemy is in attack range but there could still be an obstacle blocking it; designers didn't address this in their behavior chart?
+        //TODO: there is a flaw where enemy is in attack range but there could still be an obstacle blocking it; this wasn't addressed in behavior chart?
 
         //by now, enemy is in range to start lunge attack
         State = EnemyState.Attack;
@@ -119,23 +117,24 @@ public class LungingMeleeEnemyController : MonoBehaviour
         //pause for windup
         controller.velocity = Vector2.zero;
         yield return new WaitForSeconds(attackWindup);
-
+        
         //begin lunge now that windup is over
         //record direction to lunge towards
         Vector2 lungeDirection = directionToPlayer();
 
         //TODO: groundCharacterController seems to be a velocity-based system and has no space for forces?
-        //TODO: temporary way is to use a decreasing timer and have speed fall linearly with it
+        //current way of lunging is to use a decreasing timer and have speed fall linearly with it
+        //Note: total distance traveled throughout this lunge is about 0.5 * lungeTopSpeed * lungeDuration
         timer = lungeDuration;
         while (timer > 0)
         {
             controller.velocity = lungeDirection * (lungeTopSpeed * timer / lungeDuration);
             yield return null;
         }
+        //TODO: designers want the damaging hitbox on top of the enemy when it throws itself; might want to wait until we see the assets/visual
 
-
-        //TODO: is attack cooldown for being still or it can still move during then, just can't attack? going with former for now
-        //done attacking, now leave a cooldown of 2s
+        //TODO: is attack cooldown for being still before moving again or it can still move during then, just can't attack? going with former for now
+        //done attacking, now leave a cooldown of being still
         controller.velocity = Vector2.zero;
         yield return new WaitForSeconds(attackCooldown);
 
@@ -143,15 +142,27 @@ public class LungingMeleeEnemyController : MonoBehaviour
         State = EnemyState.MoveToPlayer;
     }
 
-    //TODO: go around obstacle
+    //move until an obstacle is avoided
     IEnumerator avoidObstacle()
     {
-        //TODO: avoid obstacle
+        //check for obstacle
+        Vector2 playerDirection = directionToPlayer();
+        bool obstacleInFront = Physics2D.Raycast(this.transform.position, playerDirection, attackRange, LayerMask.GetMask("Obstacle"));
 
-        //TODO: take out behavior below; it just mimics normal moving for now until actual avoidance is implemented
-        controller.velocity = directionToPlayer() * movementSpeed;
-        yield return new WaitForSeconds(0.0f);
+        while (obstacleInFront)
+        {
+            //use a helper function to determine which direction to avoid obstacle based on the current direction to player
+            controller.velocity = directionToAvoidObstacle(playerDirection) * movementSpeed;
 
+            //briefly move in that direction around the obstacle
+            yield return new WaitForSeconds(0.5f);
+
+            //check direction and obstacle in front again
+            playerDirection = directionToPlayer();
+            obstacleInFront = Physics2D.Raycast(this.transform.position, playerDirection, attackRange, LayerMask.GetMask("Obstacle"));
+        }
+
+        //no more obstacle in front, go back to moving
         State = EnemyState.MoveToPlayer;
     }
 
@@ -167,5 +178,28 @@ public class LungingMeleeEnemyController : MonoBehaviour
     {
         Vector2 d = PlayerController.Instance.transform.position - this.transform.position;
         return d.normalized;
+    }
+
+
+    //TODO: improve on the current method on finding a direction to avoid an obstacle; current method just tries 4 directions to see if any are clear
+    //check 45 and 90 degrees clockwise/counterclockwise directions of playerDirection
+    //helper function that gives a suggested direction to avoid an obstacle
+    Vector2 directionToAvoidObstacle(Vector2 playerDirection)
+    {
+        int[] angles = { 45, -45, 90, -90 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            //check a direction that is an angle of angles[i] away from playerDirection
+            Vector2 newDirection = Quaternion.Euler(0, 0, angles[i]) * (Vector3)playerDirection;
+            bool directionIsClear = !Physics2D.Raycast(this.transform.position, newDirection, attackRange, LayerMask.GetMask("Obstacle"));
+            if (directionIsClear)
+            {
+                return newDirection;
+            }
+        }
+        
+        //at this point, none of the 4 suggested directions work, we'll just have the enemy back up for space
+        return playerDirection * -1;
     }
 }
